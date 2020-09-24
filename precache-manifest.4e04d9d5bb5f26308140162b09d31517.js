c5e1387bdf29b0609f4d33ea241bec1494749432
AndreiManya <1234and95@gmail.com>
2020-09-24 17:29:30 +0300

    Updates

---
+++ precache-manifest.4e04d9d5bb5f26308140162b09d31517.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37af3edb2bdb53d0202c580b58cbc3a",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "a892055dd1d68fc95a38",
+    "url": "/react-app/static/css/2.6e90b12a.chunk.css"
+  },
+  {
+    "revision": "a014a06518e4c764f2f4",
+    "url": "/react-app/static/css/main.7689019f.chunk.css"
+  },
+  {
+    "revision": "a892055dd1d68fc95a38",
+    "url": "/react-app/static/js/2.6b27c7ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.6b27c7ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a014a06518e4c764f2f4",
+    "url": "/react-app/static/js/main.a7561e0d.chunk.js"
+  },
+  {
+    "revision": "605cf62042abf25f1d08",
+    "url": "/react-app/static/js/runtime-main.056c36fa.js"
+  }
+]);
